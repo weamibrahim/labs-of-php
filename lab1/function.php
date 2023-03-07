7653dd8b7868389777a159d917ad72397e8ab652
weam_ibrahim <122307908+weamibrahim@users.noreply.github.com>
2023-03-07 12:53:29 +0200

    Add files via upload

---
+++ lab1/function.php
@@ -0,0 +1,52 @@
+<?php
+function validate_form(){
+$name=isset($_POST["name"])?$_POST["name"]:"";
+$email=isset($_POST["email"])?$_POST["email"]:"";
+$message=isset($_POST["message"])?$_POST["message"]:"";
+
+
+if(empty($name)||empty($email)||empty($message)){
+
+    return" <h1 style='color:red; text-align:center '>field is empty</h1> ";
+}
+if(strlen($name) >  __MAX_NAME_LENGTH_){
+    return "<h1 style='color:red; text-align:center '>name length is not right</h1>";
+}
+elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+    return "<h1 style='color:red; text-align:center '>email is not correct</h1>";
+
+}
+elseif(strlen($message) > __MAX_MASSAGE_LENGTH_){
+   return"<h1 style='color:red; text-align:center '>massge length is not right</h1>";
+}
+
+else{
+  return "";
+}
+}
+function remember_variable($var){
+    if(isset($_POST[$var])&&!empty($_POST[$var])){
+    
+    return $_POST[$var];
+    
+    }
+    
+    else{
+        return "" ;
+    }
+    
+    }
+function print_information(){
+  echo  "<h3 style='color:red; text-align:center;margin-top:30px ; font-size:30px' >".__THANKYOU__."</h3>";
+ 
+    foreach($_POST as $key=>$value){
+        if($key!="submit"){
+        $value =trim(strtolower($value));
+        echo "<br/><strong style=' display:flex ;font-size:30px;
+        justify-content: center;'  >$key  :   $value </strong>" ;
+        }}
+}
+
+
+
+?>
